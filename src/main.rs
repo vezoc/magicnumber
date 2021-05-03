d0a33e6c1d749b1f0f95d303046b5c53a0e57360
Admin <admin@Lucas-old-Mac-Pro.local>
2021-05-02 23:44:50 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    
+    print!("{}", "Randon number between 0 and 255: ");
+    println!("{}", gen(255));
+    print!("{}", "Randon number between 0 and 231: ");
+    println!("{}", gen(231));
+       
+}
+// Solution using 16 bit integers, works for both values. 
+fn gen(a: u16) -> u16 {
+    
+    let mut val = a+1;
+    
+    while val >=a+1 {
+
+        // initiate empty string
+        let mut binstring = String::new();
+        
+        // looping for 8 random bits
+        for _i in 0..8{
+            // transforming our 0 or 1 bit into a string
+            let x = magic_num::magic_number().to_string();
+            // y is the binstring of the previous iteration
+            let y = binstring;
+            
+        // adding our next random bit to the end of binstring
+        binstring = [y,x].join("");
+        }
+        // convert binary string back into integer for comparison
+        // if val is still greater than a + 1, do the loop again
+        // until we get a value between 0 and a, and return it
+        val = u16::from_str_radix(&binstring, 2).unwrap();
+    }
+    return val;
+
+}
+
+
+
+
